e56ced86cbaec77c49d01335415fb43d94d0417a
newslive458-jpg <newslive458@gmail.com>
2025-11-11 02:15:33 +0600

    Update db.js

--- db.js
+++ db.js
@@ -1,24 +1,10 @@
-// db.js
 import pg from "pg";
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString=process.env.DATABASE_URL || "postgresql://neondb_owner:npg_TRjm3wE8BIYG@ep-nameless-tree-a1e99t7d-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require";
 
-if (!connectionString) {
-  console.error("FATAL ERROR: DATABASE_URL environment variable is not set.");
-}
-
-const pool = new pg.Pool({
-  connectionString: connectionString,
-  ssl: { rejectUnauthorized: false },
-});
-
-// Test connection
-pool.query('SELECT 1 + 1 AS result', (err, res) => {
-    if (err) {
-        console.error('Database connection test failed:', err.stack);
-    } else {
-        console.log('Database connection successful. Test result:', res.rows[0].result);
-    }
+const pool=new pg.Pool({
+  connectionString,
+  ssl:{ rejectUnauthorized:false }
 });
 
 export default pool;
