d0f2aefbc76c0a3c1f26b0e99d9273ea54b96734
newslive458-jpg <newslive458@gmail.com>
2025-11-11 06:48:29 +0600

    Delete config directory

--- config/config.js
+++
@@ -1,8 +0,0 @@
-// Monetag Zone ID, Telegram Bot ID, PostgreSQL DATABASE_URL
-
-export const MONETAG_ZONE_ID = '10070523'; // Monetag Zone ID
-export const TELEGRAM_BOT_USERNAME = '@EarnQuick_Official_bot';
-export const TELEGRAM_ADMIN_ID = 8145444675; // Admin Telegram ID
-export const POSTGRES_DB_URL = 'postgresql://neondb_owner:npg_TRjm3wE8BIYG@ep-nameless-tree-a1e99t7d-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require';
-export const WITHDRAW_METHODS = ['বিকাশ', 'নগদ', 'রকেট', 'USDT'];
-export const WITHDRAW_POINTS = 5000; // Minimum points for withdrawal
